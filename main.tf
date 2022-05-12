4e6fb71a96c0de753da0d5fe7593e63c6113efac
Mohammad2403 <105421671+Mohammad2403@users.noreply.github.com>
2022-05-12 14:28:42 +0530

    rds

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "aws_db_instance" "default" {
+allocated_storage = 20
+identifier = "testinstance"
+storage_type = "gp2"
+engine = "mysql"
+engine_version = "5.7"
+instance_class = "db.m4.large"
+name = "test"
+username = "admin"
+password = "Admin@54132"
+parameter_group_name = "default.mysql5.7"
+}resource "aws_db_instance" "default" {
+allocated_storage = 20
+identifier = "testinstance"
+storage_type = "gp2"
+engine = "mysql"
+engine_version = "5.7"
+instance_class = "db.m4.large"
+name = "test"
+username = "admin"
+password = "Admin@54132"
+parameter_group_name = "default.mysql5.7"
+}
