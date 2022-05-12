80d6ce24386c871113af06dd65ba899814bd9688
Mohammad2403 <105421671+Mohammad2403@users.noreply.github.com>
2022-05-12 14:48:38 +0530

    security group

--- main.tf
+++ main.tf
@@ -1,23 +1,26 @@
-resource "aws_db_instance" "default" {
-allocated_storage = 20
-identifier = "testinstance"
-storage_type = "gp2"
-engine = "mysql"
-engine_version = "5.7"
-instance_class = "db.m4.large"
-name = "test"
-username = "admin"
-password = "Admin@54132"
-parameter_group_name = "default.mysql5.7"
-}resource "aws_db_instance" "default" {
-allocated_storage = 20
-identifier = "testinstance"
-storage_type = "gp2"
-engine = "mysql"
-engine_version = "5.7"
-instance_class = "db.m4.large"
-name = "test"
-username = "admin"
-password = "Admin@54132"
-parameter_group_name = "default.mysql5.7"
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.main.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
 }
