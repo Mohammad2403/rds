d884f60c232d86a271ebe53fbdcb8dfd49906278
Mohammad2403 <105421671+Mohammad2403@users.noreply.github.com>
2022-05-12 14:56:15 +0530

    ec2

--- main.tf
+++ main.tf
@@ -1,31 +1,5 @@
-data "aws_subnet_ids" "subnets" {
-  vpc_id = var.vpc_id
-}
- 
-data "aws_subnet" "subnet_values" {
-  for_each = data.aws_subnet_ids.subnets.ids
-  id       = each.value
-}
- 
-resource"aws_launch_configuration" "launch-configuration" {
-  name = var.launch_configuration_name
-  image_id = var.image_id
-  instance_type = var.instance_type
-}
- 
-resource "aws_autoscaling_group" "autoscalling_group_config" {
-  name = var.auto_scalling_group_name
-  max_size = 3
-  min_size = 2
-  health_check_grace_period = 300
-  health_check_type = "EC2"
-  desired_capacity = 3
-  force_delete = true
-  vpc_zone_identifier = [for s in data.aws_subnet.subnet_values: s.id]
- 
-  launch_configuration = aws_launch_configuration.launch-configuration.name
- 
-  lifecycle {
-    create_before_destroy = true
-  }
+
+resource "aws_instance" "terraform-example"{
+ami = "ami-0022f774911c1d690"
+instance_type = "t2.micro"
 }
